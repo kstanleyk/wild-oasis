d6dc53e67711e2ea0fe8c0f885794a2012509057
Stanley Kiven <kstanleyk@live.com>
2024-01-08 09:20:07 +0100

    refactor cabin service

--- src/services/cabin.service.ts
+++ src/services/cabin.service.ts
@@ -1,11 +1,13 @@
 import { apiRequests } from '.'
 import { Cabin, CabinResponse } from '../features/cabins/cabin.model'
 
+const route = "/cabins"
+
 const cabinService = {
-    list: (): Promise<Cabin[]> => apiRequests.get('/cabins'),
-    details: (code: string): Promise<Cabin> => apiRequests.get(`/cabins/${code}`),
-    create: (branch: Cabin): Promise<CabinResponse> => apiRequests.post('/cabins', branch),
-    update: (branch: Cabin): Promise<CabinResponse> => apiRequests.put('/cabins', branch),
+    list: (): Promise<Cabin[]> => apiRequests.get(route),
+    details: (code: string): Promise<Cabin> => apiRequests.get(`${route}/${code}`),
+    create: (cabin: Cabin): Promise<CabinResponse> => apiRequests.post(route, cabin),
+    update: (cabin: Cabin): Promise<CabinResponse> => apiRequests.put(route, cabin),
 }
 
 export default cabinService
